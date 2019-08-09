d6fff238b5114081ba13e1d47aa31e2d5339890c
DarekR88 <dradmclad@gmail.com>
2019-08-09 13:54:06 -0500

    projects updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,51 +10,51 @@ const Nav = props => (
             </div>
             <div className='navItems'>
                 <p className='homeLink'>
-                <Link
-                activeClass="active"
-                to="home"
-                spy={true}
-                smooth={true}
-                offset={-200}
-                duration={500}
-              >
-                Home
+                    <Link
+                        activeClass="active"
+                        to="home"
+                        spy={true}
+                        smooth={true}
+                        offset={-200}
+                        duration={500}
+                    >
+                        Home
               </Link>
                 </p>
                 <p className='aboutLink'>
-                <Link
-                activeClass="active"
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={-150}
-                duration={500}
-              >
-                About
+                    <Link
+                        activeClass="active"
+                        to="about"
+                        spy={true}
+                        smooth={true}
+                        offset={-150}
+                        duration={500}
+                    >
+                        About
               </Link>
                 </p>
                 <p className='projectsLink'>
-                <Link
-                activeClass="active"
-                to="projects"
-                spy={true}
-                smooth={true}
-                offset={-150}
-                duration={500}
-              >
-                Projects
+                    <Link
+                        activeClass="active"
+                        to="projects"
+                        spy={true}
+                        smooth={true}
+                        offset={-150}
+                        duration={500}
+                    >
+                        Projects
               </Link>
                 </p>
-                <p className='contactLink'>              
-                <Link
-                activeClass="active"
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={-150}
-                duration={500}
-              >
-                Contact
+                <p className='contactLink'>
+                    <Link
+                        activeClass="active"
+                        to="contact"
+                        spy={true}
+                        smooth={true}
+                        offset={-150}
+                        duration={500}
+                    >
+                        Contact
               </Link>
 
                 </p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,13 +2,26 @@ import React from 'react';
 import GamejamImage from '../gameJam.PNG';
 import Scraper from '../scrape.PNG';
 import Hair from '../hair.PNG';
+import HerokuIcon from '../heroku-icon-27.jpg'
 
 const Projects = props => (
     <div className='projectsContainer' id='projects'>
         <div className='projects'>
             <p className='projectHeader'>Projects</p>
             <div className='bestProject'>
-                <img className='projectImage' src={Hair} alt="Hair screenshot" />
+                <div className='projectButtons'>
+                    <img className='projectImage' src={Hair} alt="Hair screenshot" />
+                    <div className='projectLinks'>
+                        <a className='deployedLink' href={'https://docs.google.com/document/d/1rnKLGv9Pz8hbtlZFnhmpjAVmRgtdeURjkIdtgBEY_b4/edit?usp=sharing'} target={'_blank'}>
+                            <span><img className='herokuIcon' src={HerokuIcon} alt='herokuIcon'></img></span>
+                            Deployed Link
+                        </a>
+                        <a className='githubLink' href={'https://github.com/Emilykradke/hair-salon-julie'} target={'_blank'}>
+                            <span className='fab fa-github' id='githubIcon'></span>
+                            GitHub Link
+                        </a>
+                    </div>
+                </div>
                 <div className='bestProjectDescription'>
                     <p className='projectTitle'>
                         Hair by Julie G
@@ -18,10 +31,10 @@ const Projects = props => (
                     </p>
                     <ul>
                         <li>
-                            Custom, fully mobile responsive front end created with React, Sass, JSX, and Javascript. 
+                            Custom, fully mobile responsive front end created with React, Sass, JSX, and Javascript.
                         </li>
                         <li>
-                            MongoDB, and Mongoose database allows an administrator to customize hours, services, 
+                            MongoDB, and Mongoose database allows an administrator to customize hours, services,
                             contact information, and location.
                         </li>
                     </ul>
@@ -29,7 +42,20 @@ const Projects = props => (
             </div>
             <div className='divider'></div>
             <div className='projectOne'>
-                <img className='projectImage' src={GamejamImage} alt="gamejam screenshot" />
+                <div className='projectButtons'>
+                    <img className='projectImage' src={GamejamImage} alt="gamejam screenshot" />
+                    <div className='projectLinks'>
+                        <a className='deployedLink' href={'https://game-jam2019.herokuapp.com/'} target={'_blank'}>
+                            <span><img className='herokuIcon' src={HerokuIcon} alt='herokuIcon'></img></span>
+                            Deployed Link
+                        </a>
+                        <a className='githubLink' href={'https://github.com/lydmgem/Project2'} target={'_blank'}>
+                            <span className='fab fa-github' id='githubIcon'></span>
+                            GitHub Link
+                        </a>
+                    </div>
+
+                </div>
                 <div className='projectOneDescription'>
                     <p className='projectTitle'>
                         Game Jam
@@ -50,7 +76,19 @@ const Projects = props => (
             </div>
             <div className='divider'></div>
             <div className='projectTwo'>
-                <img className='projectImage' src={Scraper} alt="mongo scraper screenshot" />
+                <div className='projectButtons'>
+                    <img className='projectImage' src={Scraper} alt="mongo scraper screenshot" />
+                    <div className='projectLinks'>
+                        <a className='deployedLink' href={'https://mysterious-woodland-91607.herokuapp.com/'} target={'_blank'}>
+                            <span><img className='herokuIcon' src={HerokuIcon} alt='herokuIcon'></img></span>
+                            Deployed Link
+                        </a>
+                        <a className='githubLink' href={'https://github.com/DarekR88/ScrapeToMongo'} target={'_blank'}>
+                            <span className='fab fa-github' id='githubIcon'></span>
+                            GitHub Link
+                        </a>
+                    </div>
+                </div>
                 <div className='projectTwoDescription'>
                     <p className='projectTitle'>
                         Web Scraper With Mongo Database
@@ -64,7 +102,7 @@ const Projects = props => (
                             Fully custom front end constructed with HTML, CSS, JavaScript, and JQuery.
                         </li>
                         <li>
-                            Node, Express, Axios, Mongoose, and Cheerio back end with a MongoDB database. 
+                            Node, Express, Axios, Mongoose, and Cheerio back end with a MongoDB database.
                             Users can save articles that interest them and also save notes to them.
                         </li>
                     </ul>
