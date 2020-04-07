fe9b05d2ef9c8f9ac25ef08f8b489324c982a43f
DarekR88 <dradmclad@gmail.com>
2020-04-06 19:47:04 -0500

    widths and margins

--- src/components/About.js
+++ src/components/About.js
@@ -4,17 +4,17 @@ const About = props => (
     <div className='aboutContainer' id='about'>
         <p className='aboutTitle'>About</p>
         <div className='about'>
-            <p className='summary'>
+            <p className='about-block'>
                 UI/UX developer with a passion for creating dynamic and responsive experiences.
                 I enjoy working with and creating APIs and I have experience working with MongoDB and SQL databases.
                 Programming full stack applications in javaScript and working with React, Angular, and Vue.js are areas that I excel in.
             </p>
-            <p className='backEnd'>
+            <p className='about-block'>
                 I am well versed in the methods and technoligies utilized in creating a modern back end. Express, Node.js, MongoDB,
                 Mongoose, MySQL, and Sequelize are technologies that I have employed in the development of the projects I have completed.
                 I have experience creating APIs from scratch and using third party APIs to deveolop dynamic applications.
             </p>
-            <p className='technologies'>
+            <p className='about-block'>
                 Learning is a very important part of development. New and exciting technologies are constantly being created.
                 Being an excellent developer and programmer hinges on my ability to adapt to new technologies and determine
                 what best fits my needs for a particular project.
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,66 +1,63 @@
-import React from 'react';
-import Logo from '../newTransparentLogo.png'
+import React from "react";
+import Logo from "../newTransparentLogo.png";
 import { Link } from "react-scroll";
 
-const Nav = props => (
-    <div className='navContainer'>
-        <div className='nav'>
-            <div className='logo'>
-                <img className='dLogo' src={Logo} alt="D logo" />
-            </div>
-            <div className='navItems'>
-                <p className='homeLink'>
-                    <Link
-                        activeClass="active"
-                        to="home"
-                        spy={true}
-                        smooth={true}
-                        offset={-200}
-                        duration={500}
-                    >
-                        Home
-              </Link>
-                </p>
-                <p className='aboutLink'>
-                    <Link
-                        activeClass="active"
-                        to="about"
-                        spy={true}
-                        smooth={true}
-                        offset={-150}
-                        duration={500}
-                    >
-                        About
-              </Link>
-                </p>
-                <p className='projectsLink'>
-                    <Link
-                        activeClass="active"
-                        to="projects"
-                        spy={true}
-                        smooth={true}
-                        offset={-150}
-                        duration={500}
-                    >
-                        Projects
-              </Link>
-                </p>
-                <p className='contactLink'>
-                    <Link
-                        activeClass="active"
-                        to="contact"
-                        spy={true}
-                        smooth={true}
-                        offset={-125}
-                        duration={500}
-                    >
-                        Contact
-              </Link>
-
-                </p>
-            </div>
-        </div>
+const Nav = (props) => (
+  <div className="nav">
+    <div className="logo">
+      <img className="dLogo" src={Logo} alt="D logo" />
     </div>
+    <ul className="navItems">
+      <li className="nav-item home">
+        <Link
+          activeClass="active"
+          to="home"
+          spy={true}
+          smooth={true}
+          offset={-200}
+          duration={500}
+        >
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          activeClass="active about"
+          to="about"
+          spy={true}
+          smooth={true}
+          offset={-150}
+          duration={500}
+        >
+          About
+        </Link>
+      </li>
+      <li className="nav-item projects">
+        <Link
+          activeClass="active"
+          to="projects"
+          spy={true}
+          smooth={true}
+          offset={-150}
+          duration={500}
+        >
+          Projects
+        </Link>
+      </li>
+      <li className="nav-item contact">
+        <Link
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-125}
+          duration={500}
+        >
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </div>
 );
 
 export default Nav;
